8fac6554f363eab076f0cf4676e42a56a34fb15f
mr.Escow <escowxbox@gmail.com>
2021-03-08 00:09:14 +0300

    создал файл инициализации

---
+++ Initialization.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+std::string nameDirectory = "resources";
+
+std::string nameFile = "file_1.txt";
+
+std::ofstream fileOne;
--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
-
 #include <fstream>
-//#include <ifstream>
-//#include <ofstream>
-
 #include <direct.h>
 #include <string>
 
+#include "Initialization.h"
 
 
 
@@ -14,13 +11,7 @@ int main() {
 
 	setlocale(LC_ALL, "ru");
 
-	std::string nameDirectory = "resources";
-
 	_mkdir(nameDirectory.c_str());
-
-	std::ofstream fileOne;
-
-	std::string nameFile= "file_1.txt";
 
 	fileOne.open(nameDirectory+"\\"+nameFile);
 
